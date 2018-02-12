991033aabb6ab9aeeb534cac155dbddc135c43e3
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-12 01:40:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
-  
 }
+function destructivelyRemoveElementFromEndOfArray(array)
