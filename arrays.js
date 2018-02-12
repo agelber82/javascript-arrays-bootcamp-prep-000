edfd59352431687da48a431a552214d0f43e90d0
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-12 01:42:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,7 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-  
+  array.pop();
+  return array;
 }
+function removeElementFromEndOfArray(array)
