18693c112a17ded9e23ed7f2c9ad03e62cfc45ed
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-12 00:54:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  return;
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return;
+}
