5b7e1967f6c065af7cb1c03882f55c39defb08c0
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-12 01:02:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray(chocolatebars, element) {
   return;
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
